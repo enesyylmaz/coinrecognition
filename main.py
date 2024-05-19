e6c7742ed771c51fe71750aabb7f159f7bd52a2e
enesyylmaz <98919812+enesyylmaz@users.noreply.github.com>
2024-05-19 19:25:35 +0300

    image processing

--- main.py
+++ main.py
@@ -17,34 +17,37 @@ def preprocessimage(image):
 
 	org = cv2.imdecode(np.frombuffer(image, np.uint8), -1)
 	image = cv2.cvtColor(org, cv2.COLOR_BGR2GRAY)
-	image = cv2.blur(image, (7, 7))
+	image = cv2.blur(image, (7,7))
 	orgGray = np.array(image, copy=True)
 
-	height, width = orgGray.shape[:2]
+	height, width = image.shape[:2]
 	circles = None
-	while (circles is None) and (param2Value > 50):
+	while (circles is None) and (param2Value > 50) :
 		image = np.array(orgGray, copy=True)
-		circles = cv2.HoughCircles(image, cv2.HOUGH_GRADIENT, 1, 40,
-									param1=60, param2=param2Value, minRadius=0, maxRadius=0)
+		circles = cv2.HoughCircles(image,cv2.HOUGH_GRADIENT,1,40,
+								param1=60,param2=param2Value,minRadius=0,maxRadius=0)
 		param2Value -= param2Change
-
+	
 	if circles is not None:
 		original = np.array(circles, copy=True)
 		circles = np.uint16(np.around(circles))
 		mask = np.zeros_like(image)
 
-		maxCircle = max(circles[0, :], key=lambda x: x[2])
+		maxCircle = max(circles[0, :], key = lambda x: x[2])
 		radius = int(maxCircle[2] * offset)
 		cv2.circle(mask, (maxCircle[0], maxCircle[1]), radius, (255, 255, 255), thickness=-1)
-
+	
 		top = int(maxCircle[1] - radius)
 		bottom = int(maxCircle[1] + radius)
 		left = int(maxCircle[0] - radius)
 		right = int(maxCircle[0] + radius)
-
+		
 		result = cv2.bitwise_and(org, org, mask=mask)
 		isolated = result[top:bottom, left:right]
 
+		isolated = cv2.cvtColor(isolated, cv2.COLOR_BGR2RGB)
+		isolated = cv2.resize(isolated, (256,256))
+		  
 		_, img_encoded = cv2.imencode('.jpg', isolated)
 		return img_encoded.tobytes()
 	else:
